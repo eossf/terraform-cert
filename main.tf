38e55b5e4a9d0531a15afcd87612ad97baddf53a
Ubuntu <ubuntu@ip-172-31-18-6.eu-west-3.compute.internal>
2021-07-25 07:24:08 +0000

    output function...

--- main.tf
+++ main.tf
@@ -1,9 +1,62 @@
+# Set the required provider and versions
 terraform {
   required_providers {
+    # We recommend pinning to the specific version of the Docker Provider you're using
+    # since new versions are released frequently
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
+      version = "2.14.0"
     }
   }
 }
 
-provider "docker" {}
+# Configure the docker provider
+provider "docker" {
+}
+
+resource "random_string" "random" {
+  length = 4
+  special = false
+}
+
+# Create a docker image resource
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+resource "docker_image" "nodered" {
+  name         = "nodered/node-red:latest"
+  keep_locally = true
+}
+
+# Create a docker container resource
+resource "docker_container" "nodered" {
+  name    = join("-", ["nodered", random_string.random.result])
+  image   = docker_image.nodered.latest
+
+  ports {
+    external = 1880
+    internal = 1880
+  }
+}
+
+resource "docker_container" "nginx" {
+  name    = "nginx"
+  image   = docker_image.nginx.latest
+
+  ports {
+    external = 8080
+    internal = 80
+  }
+}
+
+output "IP-Address" {
+  value = join(":", [docker_container.nodered.ip_address, docker_container.nodered.ports[0].internal])
+  description = "The IP address:port of the container"
+}
+
+output "container-name" {
+  value = docker_container.nodered.name
+  description = "The name of the container"
+}
