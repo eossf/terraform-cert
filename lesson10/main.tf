42bc9d3374ada2037692d65854b642de6413cc18
Ubuntu <ubuntu@ip-172-31-18-6.eu-west-3.compute.internal>
2021-07-26 15:09:05 +0000

    lesson 10

---
+++ lesson10/main.tf
@@ -0,0 +1,53 @@
+# Set the required provider and versions
+terraform {
+  required_providers {
+    # We recommend pinning to the specific version of the Docker Provider you're using
+    # since new versions are released frequently
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.14.0"
+    }
+  }
+}
+
+# variable must be input or set when running terraform
+# could be export TF_VAR_internal_port=
+variable "internal_port" {
+  type = list
+  default = [1880, 1881, 1882, 1883]
+}
+
+variable "container_count" {
+  type = number
+  default = 4
+}
+
+# Configure the docker provider
+provider "docker" {
+}
+
+resource "random_string" "random" {
+  length = 4
+  special = false
+  count = var.container_count
+}
+
+resource "docker_image" "nodered" {
+  name         = "nodered/node-red:latest"
+  keep_locally = true
+}
+
+# Create a docker container resource
+resource "docker_container" "nodered" {
+  count = var.container_count
+  name    = join("-", ["nodered", random_string.random[count.index].result])
+  image   = docker_image.nodered.latest
+  ports {
+    internal = min(var.internal_port[count.index],1880)
+  }
+}
+
+output "container-name" {
+  value = docker_container.nodered[*].name
+  description = "The name of the container"
+}
---
+++ lesson10/readme.md
@@ -0,0 +1,22 @@
+# Local Variables and misc function
+
+## common steps before starting
+
+Model file is replacing the main.tf file for starting the exercise without the solution
+
+```bash
+alias t='terraform'
+alias ll='ls -ltrhA'
+t destroy
+t init
+cp -f model main.tf
+````
+
+## remove manually resource
+- change the internal_port to list type, set two elements in the array: 1880, 1881 ...
+- set value container_count to : 4
+- add a max function with 1880
+- add a min , still 1880
+
+## Outcome
+just do a plan to verify 
