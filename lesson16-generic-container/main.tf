0d8fe65918b87bf58c3777a8fad40272de879919
Ubuntu <ubuntu@ip-172-31-18-6.eu-west-3.compute.internal>
2021-08-22 13:31:04 +0000

    cleaning

--- lesson16-generic-container/main.tf
+++ lesson16-generic-container/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.14.0"
-    }
-  }
-}
-
 resource "random_string" "random" {
   length = 4
   special = false
--- lesson16-generic-container/readme.md
+++ lesson16-generic-container/readme.md
@@ -1,1 +1,6 @@
 # complete module docker 
+
+## container module
+## image module
+
+Just do terraform init, plan, apply
