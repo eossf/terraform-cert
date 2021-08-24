9c3938387a06563e0267ad71c6764fefc4904488
Ubuntu <ubuntu@ip-172-31-18-6.eu-west-3.compute.internal>
2021-08-24 18:19:28 +0000

    clean

--- lesson19-imgXcnt/main.tf
+++ lesson19-imgXcnt/main.tf
@@ -1,14 +1,14 @@
 locals {
   deployment = {
     nginx = {
-      container_count = length(var.external_port[terraform.workspace])
+      container_count = length(var.external_port["nginx"][terraform.workspace])
       image          = var.image_name["nginx"][terraform.workspace]
       internal_port  = var.internal_port
       external_port  = var.external_port["nginx"][terraform.workspace]
       container_path = "/data"
     }
     influxdb = {
-      container_count = length(var.external_port[terraform.workspace])
+      container_count = length(var.external_port["influxdb"][terraform.workspace])
       image          = var.image_name["influxdb"][terraform.workspace]
       internal_port  = var.internal_port
       external_port  = var.external_port["influxdb"][terraform.workspace]
