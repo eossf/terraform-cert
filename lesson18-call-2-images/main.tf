f9f957c7935200524433b99bc209857b4feaf3fa
Ubuntu <ubuntu@ip-172-31-18-6.eu-west-3.compute.internal>
2021-08-24 18:25:30 +0000

    clean

--- lesson18-call-2-images/main.tf
+++ lesson18-call-2-images/main.tf
@@ -1,25 +1,20 @@
 locals {
   deployment = {
     nginx = {
+      container_count = length(var.external_port["nginx"][terraform.workspace])
       image = var.image_name["nginx"][terraform.workspace]
       internal_port = var.internal_port
       external_port = var.external_port["nginx"][terraform.workspace]
       container_path= "/data"
     }
     influxdb = {
+      container_count = length(var.external_port["influxdb"][terraform.workspace])
       image = var.image_name["influxdb"][terraform.workspace]
       internal_port = var.internal_port
       external_port = var.external_port["influxdb"][terraform.workspace]
       container_path = "/var/lib/influxdb"
     }
   }
-}
-
-resource "random_string" "random" {
-  length  = 4
-  special = false
-  upper = false
-  for_each = local.deployment
 }
 
 module "image" {
@@ -31,7 +26,8 @@ module "image" {
 module "container" {
   source                 = "./container"
   for_each               = local.deployment
-  name_in                = join("-", [each.key, terraform.workspace, random_string.random[each.key].result])
+  count_in               = each.value.container_count
+  name_in                = each.key
   image_in               = module.image[each.key].image_out
   internal_port_in       = each.value.internal_port
   external_port_in       = each.value.external_port
