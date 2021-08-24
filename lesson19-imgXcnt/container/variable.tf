24be0d8375cfd431ff87bffb68d49c280fc4cf39
Ubuntu <ubuntu@ip-172-31-18-6.eu-west-3.compute.internal>
2021-08-24 18:12:29 +0000

    lesson19

--- lesson19-imgXcnt/container/variable.tf
+++ lesson19-imgXcnt/container/variable.tf
@@ -3,3 +3,4 @@ variable "image_in" {}
 variable "internal_port_in" {}
 variable "external_port_in" {}
 variable "container_path_in" {}
+variable "count_in" {}
--- lesson19-imgXcnt/main.tf
+++ lesson19-imgXcnt/main.tf
@@ -1,24 +1,26 @@
 locals {
   deployment = {
     nginx = {
-      image = var.image_name["nginx"][terraform.workspace]
-      internal_port = var.internal_port
-      external_port = var.external_port["nginx"][terraform.workspace]
-      container_path= "/data"
+      container_count = length(var.ext_port[terraform.workspace])
+      image          = var.image_name["nginx"][terraform.workspace]
+      internal_port  = var.internal_port
+      external_port  = var.external_port["nginx"][terraform.workspace]
+      container_path = "/data"
     }
     influxdb = {
-      image = var.image_name["influxdb"][terraform.workspace]
-      internal_port = var.internal_port
-      external_port = var.external_port["influxdb"][terraform.workspace]
+      container_count = length(var.ext_port[terraform.workspace])
+      image          = var.image_name["influxdb"][terraform.workspace]
+      internal_port  = var.internal_port
+      external_port  = var.external_port["influxdb"][terraform.workspace]
       container_path = "/var/lib/influxdb"
     }
   }
 }
 
 resource "random_string" "random" {
-  length  = 4
-  special = false
-  upper = false
+  length   = 4
+  special  = false
+  upper    = false
   for_each = local.deployment
 }
 
@@ -29,11 +31,12 @@ module "image" {
 }
 
 module "container" {
-  source                 = "./container"
-  for_each               = local.deployment
-  name_in                = join("-", [each.key, terraform.workspace, random_string.random[each.key].result])
-  image_in               = module.image[each.key].image_out
-  internal_port_in       = each.value.internal_port
-  external_port_in       = each.value.external_port
-  container_path_in      = each.value.container_path
+  source            = "./container"
+  for_each          = local.deployment
+  count_in          = 
+  name_in           = join("-", [each.key, terraform.workspace, random_string.random[each.key].result])
+  image_in          = module.image[each.key].image_out
+  internal_port_in  = each.value.internal_port
+  external_port_in  = each.value.external_port
+  container_path_in = each.value.container_path
 }
